d5dea5a1c8341ace332925d72b5b70ce79da58da
abishamuzumder <abisha.muzumder@gmail.com>
2023-04-28 14:57:25 -0700

    updated preorder

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -121,6 +121,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
+		if (node == null) return;
+		System.out.println(node.data.toString());
+		preOrderRecurse(node.leftChild);
+		preOrderRecurse(node.rightChild);
 		
 	}
 	
